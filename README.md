6f8b9c872441db6e5c362dcda56e0e1343b41937
Sydney Isaacs <si047p@att.com>
2018-03-23 21:26:19 -0400

    Review Modifications

--- README.md
+++ README.md
@@ -1,8 +1,28 @@
 # Log Analysis
 
-The code is written in Python3.5
+Description
+The code is written in Python3.5.  It accesses a sql database and provides answers to 3 provided questions.  The project is designed to test understanding of using postgresql to retrieve filter and/or calculated data from  database tables.
+
+Requirements
+PostgreSQL
+Pythno3  3.5
+Vagrant
+VirtualBox
+
+Database Set-Up
+Install VirtualBox
+Install Vagrant
+Use the following configuration file https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f73b_vagrantfile/vagrantfile
 newsdata.sql database is required.
-When the code is ran it will provdide 3 outputs.
-  1. Most popular aticles in the database of all time
-  2. Most popular article authors of all time
-  3. Any days where more than 1% of requests lead to errors.
+https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip
+
+Running command
+With the database properly setup
+Start vagrant using vagrant up
+Log into vagrant using vagrant ssh
+change directory into folder containing loganlaysis.py
+Type python3 loganalysis.py to run
+When the command is run it will provide the response to the following 3 questions about the news database.
+1. Most popular articles in the database of all time
+2. Most popular article authors of all time
+3. Any days where more than 1% of requests lead to errors.
--- loganalysis.py
+++ loganalysis.py
@@ -1,40 +1,56 @@
+#!/usr/bin/python3
 # Python 3.5
 
 import psycopg2
 
 db = psycopg2.connect("dbname=news")
 c = db.cursor()
-c.execute(
-  "select articles.title,count(*) as p_views from log join articles\
-  on log.path ~ articles.slug group by articles.title order by p_views desc;")
+c.execute("""
+  SELECT articles.title,count(*) AS p_views
+  FROM log JOIN articles
+  ON log.path = '/article/'||articles.slug
+  GROUP BY articles.title
+  order by p_views DESC
+  LIMIT 3;
+""")
 mostpopular = c.fetchall()
-print("Most Popular Articles")
+print("\n1. What are the most popular articles of all time?")
 for i in mostpopular:
-    print(i)
-c.close()
-
-db = psycopg2.connect("dbname=news")
-c = db.cursor()
-c.execute(
-  "select authors.name,count(*) as num from log join articles\
-  on log.path ~ articles.slug join authors on authors.id = articles.author \
-  group by authors.name order by num desc;")
-mostpopular = c.fetchall()
-print("Most Popular Article Authors")
-for i in mostpopular:
+    i = '- "{}" -- {} views'.format(i[0], i[1])
     print(i)
 db.close()
 
 db = psycopg2.connect("dbname=news")
 c = db.cursor()
-c.execute(
-  "select to_char(time, 'Mon DD, YYYY')as Date,\
-  to_char(100*((SUM(CASE WHEN status = '404 NOT FOUND' THEN 1 ELSE 0 END))/\
-  count(status)::numeric),'99D99%') as Error from log group by Date \
-  having 1<(100*((SUM(CASE WHEN status = '404 NOT FOUND' THEN 1 ELSE 0 END))/\
-  count(status)::numeric));")
+c.execute("""
+  SELECT authors.name,count(*) AS num
+  FROM log JOIN articles
+  ON log.path = '/article/'||articles.slug
+  JOIN authors ON authors.id = articles.author
+  GROUP BY authors.name
+  ORDER BY num DESC;
+""")
+mostpopular = c.fetchall()
+print("\n2. Who are the most popular article authors of all time?")
+for i in mostpopular:
+    i = "- {} -- {} views".format(i[0], i[1])
+    print(i)
+db.close()
+
+db = psycopg2.connect("dbname=news")
+c = db.cursor()
+c.execute("""
+  SELECT to_char(time, 'Mon DD, YYYY')as Date,
+  to_char(100*((SUM(CASE WHEN status = '404 NOT FOUND' THEN 1 ELSE 0 END))/
+  count(status)::numeric),'99D99%') AS Error
+  FROM log
+  GROUP BY Date
+  HAVING 1<(100*((SUM(CASE WHEN status = '404 NOT FOUND' THEN 1 ELSE 0 END))/
+  count(status)::numeric));
+""")
 perc_error = c.fetchall()
-print("Days Experiencing > 1% in Page Errors")
+print("\n3. On which days did more than 1% of requests lead to errors?")
 for i in perc_error:
-    print(perc_error)
+    i = "- {} -- {} Errors".format(i[0], i[1])
+    print(i, "\n")
 db.close()
